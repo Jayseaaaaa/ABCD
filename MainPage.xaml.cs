2ad2048c9b07cae0db04c69973ed8899d4e25c71
Jayseaaaaa <alexzandre.deleon06@gmail.com>
2026-02-25 08:04:58 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Page_2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnSubmitClicked(object sender, EventArgs e)
+        {
+            string userName = foodEntry.Text;
+
+
+            if (!string.IsNullOrEmpty(userName))
+            {
+
+                DisplayAlert("There are no such thing like that ", $"{userName}!", "OK");
+
+            }
+            else
+            {
+                DisplayAlert("Error", "Please ENTER SOMETHING.", "OK");
+            }
+
+        }
+        private void OnImageButtonClicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert(
+                "Fried Chicken", "Calories: 140 calories for a wing " +
+                "\n340 calories for a large breast. " +
+                "\nProtein: 30 grams of protein per serving (200 grams). " +
+                "\nFat: 25 grams of fat per serving. " +
+                "\nCarbohydrates: 20 grams of carbohydrates per serving. ", "OK");
+        }
+    }
+
+}
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,13 +1,23 @@
-﻿namespace MauiApp1.WinUI
+﻿using Microsoft.Extensions.Logging;
+
+namespace Page_2
 {
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-
             builder
-                .UseSharedMauiApp();
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
 
             return builder.Build();
         }
