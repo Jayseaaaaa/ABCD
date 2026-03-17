ce82590d8de7456d33ec232f571d318f2c6e1cc1
lorez214 <cayabyab.laurencejake@gmail.com>
2026-03-17 20:30:18 +0800

    Update MainPage.xaml.cs

--- ABCD.CruzT.Kabigting.lorez214/MainPage.xaml.cs
+++ ABCD.CruzT.Kabigting.lorez214/MainPage.xaml.cs
@@ -1,25 +1,102 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Devices.Sensors;
+using System;
+using System.Timers;
+
 ﻿namespace ABCD.CruzT.Kabigting.lorez214
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private Timer _timer;
+    private int _seconds = 0;
+    private bool _isRunning = false;
+
+    private Location _lastLocation;
+    private double _distance = 0;
+
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
+    }
 
-        public MainPage()
+    private async void OnSearchPressed(object sender, EventArgs e)
+    {
+        var locations = await Geocoding.GetLocationsAsync(SearchBar.Text);
+        var location = locations?.FirstOrDefault();
+
+        if (location != null)
         {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            MainMap.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Location(location.Latitude, location.Longitude),
+                    Distance.FromKilometers(1)));
         }
     }
 
+    private void OnStartClicked(object sender, EventArgs e)
+    {
+        if (_isRunning)
+        {
+            _timer?.Stop();
+            _isRunning = false;
+            return;
+        }
+
+        _timer = new Timer(1000);
+        _timer.Elapsed += UpdateTime;
+        _timer.Start();
+
+        StartTracking();
+        _isRunning = true;
+    }
+
+    private void UpdateTime(object sender, ElapsedEventArgs e)
+    {
+        _seconds++;
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            TimeLabel.Text = TimeSpan.FromSeconds(_seconds).ToString(@"mm\:ss");
+        });
+    }
+
+    private async void StartTracking()
+    {
+        while (_isRunning)
+        {
+            var location = await Geolocation.GetLocationAsync();
+
+            if (location != null)
+            {
+                if (_lastLocation != null)
+                {
+                    var d = Location.CalculateDistance(_lastLocation, location, DistanceUnits.Kilometers);
+                    _distance += d;
+
+                    double speed = location.Speed ?? 0;
+
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        DistanceLabel.Text = _distance.ToString("0.00");
+                        SpeedLabel.Text = speed.ToString("0.0");
+                    });
+                }
+
+                _lastLocation = location;
+            }
+
+            await Task.Delay(2000);
+        }
+    }
+
+    private void OnHistoryClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("History", "Show saved routes here.", "OK");
+    }
+
+    private void OnHelpClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Help", "Instructions go here.", "OK");
+    }
 }
